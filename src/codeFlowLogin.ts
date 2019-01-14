8a12a2211704a311447349242c15a1a2b212ba77
Christof Marti <chrmarti@microsoft.com>
2019-01-14 10:42:20 +0100

    Copyright headers

--- src/codeFlowLogin.ts
+++ src/codeFlowLogin.ts
@@ -1,4 +1,8 @@
 //#!/usr/bin/env ts-node
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
 import * as http from 'http';
 import * as https from 'https';

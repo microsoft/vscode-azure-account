ea994c919af348330f7fd5cce545a18778ab749e
William Lorey <wilorey@microsoft.com>
2021-06-14 10:52:43 -0700

    Add copyright header

--- src/checkIsOnline.ts
+++ src/checkIsOnline.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { Environment } from "@azure/ms-rest-azure-env";
 import * as http from 'http';
 import * as https from 'https';

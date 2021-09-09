09bbc27256150472c6751ffaf8b4abbc400cbfc6
Will Lorey <wilorey@microsoft.com>
2021-09-09 14:55:20 -0700

    Add 'Sign In with Device Code' for MSAL (#310)

--- src/login/adal/AdalAuthProvider.ts
+++ src/login/adal/AdalAuthProvider.ts
@@ -16,7 +16,7 @@ import { ext } from "../../extensionVariables";
 import { localize } from "../../utils/localize";
 import { timeout } from "../../utils/timeUtils";
 import { AbstractCredentials, AbstractCredentials2, AuthProviderBase } from "../AuthProviderBase";
-import { getCallbackEnvironment, getUserCode, parseQuery, showDeviceCodeMessage, UriEventHandler } from "./login";
+import { getCallbackEnvironment, getUserCode, parseQuery, UriEventHandler } from "./login";
 import { addTokenToCache, clearTokenCache, deleteRefreshToken, getStoredCredentials, getTokenResponse, getTokensFromToken, getTokenWithAuthorizationCode, ProxyTokenCache, storeRefreshToken, tokenFromRefreshToken } from "./tokens";
 
 const staticEnvironmentNames: string[] = [
@@ -87,7 +87,7 @@ export class AdalAuthProvider extends AuthProviderBase<TokenResponse[]> {
 
 	public async loginWithDeviceCode(environment: Environment, tenantId: string): Promise<TokenResponse[]> {
 		const userCode: UserCodeInfo = await getUserCode(environment, tenantId);
-		const messageTask: Promise<void> = showDeviceCodeMessage(userCode);
+		const messageTask: Promise<void> = this.showDeviceCodeMessage(userCode.message, userCode.userCode, userCode.verificationUrl);
 		const tokenResponseTask: Promise<TokenResponse> = getTokenResponse(environment, tenantId, userCode);
 		const tokenResponse: TokenResponse = await Promise.race([tokenResponseTask, messageTask.then(() => Promise.race([tokenResponseTask, timeout(3 * 60 * 1000)]))]); // 3 minutes
 

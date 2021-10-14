57a5b418efcaf849a13567a59df107295cad3db2
William Lorey <wilorey@microsoft.com>
2021-10-14 12:13:18 -0700

    Call `registerUIExtensionVariables` before creating experimentation service

--- src/extensionVariables.ts
+++ src/extensionVariables.ts
@@ -3,8 +3,11 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { IExperimentationServiceAdapter } from "vscode-azureextensionui";
+import { ExtensionContext } from "vscode";
+import { IAzExtOutputChannel, IExperimentationServiceAdapter } from "vscode-azureextensionui";
 
 export namespace ext {
+    export let context: ExtensionContext;
+    export let outputChannel: IAzExtOutputChannel;
     export let experimentationService: IExperimentationServiceAdapter;
 }

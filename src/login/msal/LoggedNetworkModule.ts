c7ede2828c6f88206745931c346bd07922eb938a
phoff <phoff@microsoft.com>
2022-08-03 13:33:20 -0700

    Fill in logging for MSAL network calls.

--- src/login/msal/LoggedNetworkModule.ts
+++ src/login/msal/LoggedNetworkModule.ts
@@ -1,14 +1,39 @@
 import { INetworkModule, NetworkRequestOptions, NetworkResponse } from "@azure/msal-node";
+import { ext } from "../../extensionVariables";
 
 export class LoggedNetworkModule implements INetworkModule {
     constructor(private readonly innerModule: INetworkModule) {
     }
 
-    sendGetRequestAsync<T>(url: string, options?: NetworkRequestOptions | undefined, cancellationToken?: number | undefined): Promise<NetworkResponse<T>> {
-        return this.innerModule.sendGetRequestAsync(url, options, cancellationToken);
+    async sendGetRequestAsync<T>(url: string, options?: NetworkRequestOptions | undefined, cancellationToken?: number | undefined): Promise<NetworkResponse<T>> {
+        ext.outputChannel.appendLine(`MSAL: GET ${url}...`);
+
+        try {
+            const response = await this.innerModule.sendGetRequestAsync<T>(url, options, cancellationToken);
+
+            ext.outputChannel.appendLine(`MSAL: GET ${url} response: ${response.status}`);
+
+            return response;
+        } catch (error) {
+            ext.outputChannel.appendLine(`MSAL: GET ${url} failed: ${error}`);
+
+            throw error;
+        }
     }
 
-    sendPostRequestAsync<T>(url: string, options?: NetworkRequestOptions | undefined): Promise<NetworkResponse<T>> {
-        return this.innerModule.sendPostRequestAsync(url, options);
+    async sendPostRequestAsync<T>(url: string, options?: NetworkRequestOptions | undefined): Promise<NetworkResponse<T>> {
+        ext.outputChannel.appendLine(`MSAL: POST ${url}...`);
+
+        try {
+            const response = await this.innerModule.sendPostRequestAsync<T>(url, options);
+
+            ext.outputChannel.appendLine(`MSAL: POST ${url} response: ${response.status}`);
+
+            return response;
+        } catch (error) {
+            ext.outputChannel.appendLine(`MSAL: POST ${url} failed: ${error}`);
+
+            throw error;
+        }
     }
 }

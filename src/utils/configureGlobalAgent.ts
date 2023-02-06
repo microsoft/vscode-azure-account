dbb4e90220f864932f0fb78a24fffb6a2cb51c48
Alex Weininger <alex.weininger@live.com>
2023-02-06 12:36:03 -0800

    Add comment

--- src/utils/configureGlobalAgent.ts
+++ src/utils/configureGlobalAgent.ts
@@ -10,6 +10,7 @@ export async function configureGlobalAgent(): Promise<void> {
 
     workspace.getConfiguration('http.proxy')
 
+    // order copied from https://github.com/microsoft/vscode/blob/688900a2d618c1a731b42384d71c5b107113373e/src/vs/platform/windows/electron-main/windowImpl.ts#L919-L939
     // lowercase environment variables take precedence over uppercase ones
     process.env.GLOBAL_AGENT_HTTPS_PROXY = workspace.getConfiguration('http').get('proxy') || process.env.https_proxy || process.env.HTTPS_PROXY || process.env.http_proxy || process.env.HTTP_PROXY;
     process.env.GLOBAL_AGENT_HTTP_PROXY = workspace.getConfiguration('http').get('proxy') || process.env.https_proxy || process.env.HTTPS_PROXY || process.env.http_proxy || process.env.HTTP_PROXY;
